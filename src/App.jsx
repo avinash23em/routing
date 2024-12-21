c16da02906d95bf7af5258b8e3f8b8f00855f891
avinash23em <ap23vi@gmail.com>
2024-12-21 21:29:16 +0530

    intial

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        
-    </div>
+    <>
+     
+    </>
   )
 }
 
